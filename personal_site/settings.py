9d0cc60ab9e4cdc1c2ca0caa1b251c10d0c7f554
diciocciojoseph <jodiccioc2@gmail.com>
2020-01-22 22:30:47 -0500

    Hid secret key, this app does not have user accounts, and therefore no password hashing, so the secret key does not matter in this app, but should be out of convention.

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .keys import MY_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(57$!_l3ew+59rv+pouhrlzhx*6=di46d2n-f)y3dzn3@)uy-+'
+SECRET_KEY = MY_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
